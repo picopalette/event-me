d8675ee66e77a33e05c935bda5557afd0272982d
caped-speedster <aswinsundar17@gmail.com>
2017-06-04 17:06:02 +0530

    Added profile view with blur and circular profile pic

--- app/src/main/java/io/picopalette/apps/event_me/Fragments/ProfileFragment.java
+++ app/src/main/java/io/picopalette/apps/event_me/Fragments/ProfileFragment.java
@@ -1,13 +1,21 @@
 package io.picopalette.apps.event_me.Fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import io.picopalette.apps.event_me.MainActivity;
 import io.picopalette.apps.event_me.R;
+import jp.wasabeef.blurry.Blurry;
 
 /**
  * Created by ramkumar on 02/06/17.
@@ -23,12 +31,22 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        View v =  inflater.inflate(R.layout.fragment_profile, container, false);
+        int imageresource = getResources().getIdentifier("@drawable/logo", "drawable", getActivity().getPackageName());
+        ImageView imageView = (ImageView) v.findViewById(R.id.header_cover_image);
+
+        Bitmap logo = BitmapFactory.decodeResource(getContext().getResources(), imageresource);
+        Blurry.with(getContext()).from(logo).into(imageView);
+
+        return v;
     }
 
 }
