63514302e0bd2c6198aca2978e03718620442f23
caped-speedster <aswinsundar17@gmail.com>
2017-06-05 19:29:10 +0530

    Rectification

---
+++ app/src/main/java/io/picopalette/apps/event_me/Fragments/HomeFragment.java
@@ -0,0 +1,33 @@
+package io.picopalette.apps.event_me.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.picopalette.apps.event_me.R;
+
+
+/**
+ * Created by Aswin Sundar on 05-06-2017.
+ */
+
+public class HomeFragment extends Fragment {
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
